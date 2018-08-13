56dca9b5c3cef140086e419c104482ff61b3c203
vilrolf <vilrolf@gmail.com>
2018-08-13 23:05:04 +0200

    fixed permission levels

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const crons = require('./cron/all')
-const komada = require("komada");
-const client = new komada.Client({
+const { Client, PermLevels } = require("komada");
+const defaultPermStructure = new PermLevels()
+  .addLevel(0, false, () => true)
+  .addLevel(2, false, (client, msg) => {
+    if (!msg.guild || !msg.guild.settings.modRole) return false;
+    const modRole = msg.guild.roles.get(msg.guild.settings.modRole);
+    return modRole && msg.member.roles.has(modRole.id);
+  })
+  .addLevel(3, false, (client, msg) => {
+    if (!msg.guild || !msg.guild.settings.adminRole) return false;
+    const adminRole = msg.guild.roles.get(msg.guild.settings.adminRole);
+    return adminRole && msg.member.roles.has(adminRole.id);
+  })
+  .addLevel(4, false, (client, msg) => msg.guild && msg.author.id === msg.guild.owner.id)
+const client = new Client({
   // ownerID : "your-user-id",
   prefix: "+",
+  defaultPermStructure,
   clientOptions: {
     fetchAllMembers: false,
   },
