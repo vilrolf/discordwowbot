069aaf9cab1d5d01bcc5870be0fd67a1ab23c3d0
Viljar <vilrolf@gmail.com>
2018-09-17 11:09:33 +0200

    Fix pvp rating think

--- util/charUtil.js
+++ util/charUtil.js
@@ -1,4 +1,4 @@
 exports.getBestPvpRating = (char) => {
     const pvpRating = [char.pvp.brackets.ARENA_BRACKET_2v2.rating, char.pvp.brackets.ARENA_BRACKET_3v3.rating, char.pvp.brackets.ARENA_BRACKET_RBG.rating ]
-    return  pvpRating.sort()[pvpRating.length - 1]
+    return Math.max(...pvpRating)
 }
